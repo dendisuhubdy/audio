2d1ea1bf8ed4399ea6c6192f645638605c4b4412
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2018-10-09 10:37:58 -0700

    adding librispeech

--- torchaudio/datasets/__init__.py
+++ torchaudio/datasets/__init__.py
@@ -1,4 +1,8 @@
 from .yesno import YESNO
 from .vctk import VCTK
+from .librispeech import *
 
-__all__ = ('YESNO', 'VCTK')
+__all__ = ('YESNO', 'VCTK', 'LibriSpeechTrainClean100', 'LibriSpeechTrainClean360',
+           'LibriSpeechTrainOther500', 'LibriSpeechDevClean',
+           'LibriSpeechDevOther', 'LibriSpeechTestClean',
+           'LibriSpeechTestOther')
